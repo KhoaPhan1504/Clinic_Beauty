d7dcf95246eacee7753975d76fba72ad0941100a
KhoaPhan1504 <Khoahttt01@gmail.com>
2024-09-30 10:03:33 +0700

    Fix/Build project

--- src/views/pages/Home/Home2/Home2.tsx
+++ src/views/pages/Home/Home2/Home2.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import Slider2Component from './components/Slider2Component/Slider2Component';
 import About2Component from './components/About2Component/About2Component';
 import Core2Component from './components/Core2Component/Core2Component';
 import Layout2Component from '../../../layouts/components/Layout2Component/Layout2Component';
 
-const Home2: React.FC = () => {
+const Home2 = () => {
   return (
     <Layout2Component>
       <Slider2Component />
